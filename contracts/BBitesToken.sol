451520f55d9ac04770f686d68fa6929c5e4205ab
Leandro Matus <129975099+leandro0day@users.noreply.github.com>
2023-10-10 10:49:15 -0600

    Update BBitesToken.sol

--- contracts/BBitesToken.sol
+++ contracts/BBitesToken.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
 /// @custom:security-contact lee.marreros@blockchainbites.co
-contract BBitesToken is ERC20, Pausable, AccessControl {
+contract BBitesToken is
+    ERC20Upgradeable,
+    OwnableUpgradeable,
+    AccessControlUpgradeable,
+    PausableUpgradeable,
+    UUPSUpgradeable,
+    ERC20PermitUpgradeable
+{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    constructor() ERC20("BBites Token", "BBTKN") {}
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
@@ -20,11 +29,36 @@ contract BBitesToken is ERC20, Pausable, AccessControl {
         _unpause();
     }
 
+    function initialize() public initializer {
+        /*Para que la cuenta que esta haciendo el despliegue quede 
+    como la cuenta duena de esta*/
+        __Ownable_init_unchained();
+        /*Inicializamos el contato del proxi */
+        __UUPSUpgradeable_init();
+
+        __ERC20_init("BBites Token", "BBTKN");
+
+        __Pausable_init();
+
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+
+    function mint(
+        address to,
+        uint256 amount
+    ) public onlyRole(MINTER_ROLE) whenNotPaused {
+        _mint(to, amount);
     }
 }
