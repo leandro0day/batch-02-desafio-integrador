ae259253e4f0d6557561f43af438b5b88e7053a8
Leandro Matus <129975099+leandro0day@users.noreply.github.com>
2023-10-10 10:50:34 -0600

    Update CuyCollectionNft.sol

--- contracts/CuyCollectionNft.sol
+++ contracts/CuyCollectionNft.sol
@@ -1,29 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/MerkleProofUpgradeable.sol";
 
-contract CuyCollectionNft is ERC721, Pausable, AccessControl, ERC721Burnable {
+contract CuyCollectionNft is
+    ERC721Upgradeable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    ERC721BurnableUpgradeable
+{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     bytes32 public root;
+    mapping(address => bool) public whitelist;
 
     event Burn(address account, uint256 id);
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(
+    function initialize(
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
+    ) public initializer {
+        __ERC721_init(_name, _symbol);
+        __AccessControl_init();
+        __Pausable_init();
+        __ERC721Burnable_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -33,15 +42,34 @@ contract CuyCollectionNft is ERC721, Pausable, AccessControl, ERC721Burnable {
     function safeMint(
         address to,
         uint256 tokenId
-    ) public onlyRole(MINTER_ROLE) {}
+    ) public onlyRole(MINTER_ROLE) whenNotPaused {
+        require(tokenId >= 0 && tokenId <= 999, "ID fuera de rango");
+        _safeMint(to, tokenId);
+    }
 
     function safeMintWhiteList(
         address to,
         uint256 tokenId,
         bytes32[] calldata proofs
-    ) public {}
+    ) public whenNotPaused {
+        require(whitelist[to], "Address not whitelisted");
+        require(tokenId >= 0 && tokenId <= 999, "Token ID fuera de rango");
+        require(
+            MerkleProofUpgradeable.verify(
+                proofs,
+                root,
+                keccak256(abi.encodePacked(to, tokenId))
+            ),
+            "Invalid proof"
+        );
+        _mint(to, tokenId);
+    }
 
-    function buyBack(uint256 id) public {}
+    function buyBack(uint256 id) public whenNotPaused {
+        require(id >= 1000 && id <= 1999, "Token ID fuera de rango");
+        _burn(id);
+        emit Burn(msg.sender, id);
+    }
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
@@ -54,7 +82,12 @@ contract CuyCollectionNft is ERC721, Pausable, AccessControl, ERC721Burnable {
     // The following functions are overrides required by Solidity.
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(ERC721, AccessControl) returns (bool) {
+    )
+        public
+        view
+        override(ERC721Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
