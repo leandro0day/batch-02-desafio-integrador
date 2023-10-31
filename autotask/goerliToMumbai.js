c3ee50d47961af72dfb41b1143e95f33555d21fd
Leandro Matus <leandromatus85@gmail.com>
2023-10-31 13:29:41 -0600

    add autotask

--- autotask/goerliToMumbai.js
+++ autotask/goerliToMumbai.js
@@ -8,5 +8,25 @@ exports.handler = async function (data) {
   const payload = data.request.body.events;
 
   const provider = new DefenderRelayProvider(data);
+
+
   const signer = new DefenderRelaySigner(data, provider, { speed: "fast" });
+
+
+  var onlyEvents = payload[0].matchReasons.filter((e) => e.type === "event");
+  if (onlyEvents.length === 0) return;
+
+
+  var event = onlyEvents.filter((ev) =>
+    ev.signature.includes("PurchaseNftWithId")
+  );
+
+  var { account, id } = event[0].params;
+
+  var cuyCollectionAdd = "0x0643Bfa31a2041D580d22B505e019448d576c186";
+  var tokenAbi = ["function safeMint(address to, uint256 tokenId)"];
+  var tokenContract = new ethers.Contract(cuyCollectionAdd, tokenAbi, signer);
+  var tx = await tokenContract.safeMint(account,id);
+  var res = await tx.wait();
+  return res;
 };
--- autotask/mumbaiToGoerli.js
+++ autotask/mumbaiToGoerli.js
@@ -6,4 +6,26 @@ const {
 
 exports.handler = async function (data) {
   const payload = data.request.body.events;
+
+  const provider = new DefenderRelayProvider(data);
+
+  const signer = new DefenderRelaySigner(data, provider, { speed: "fast" });
+
+
+  var onlyEvents = payload[0].matchReasons.filter((e) => e.type === "event");
+  if (onlyEvents.length === 0) return;
+
+  var event = onlyEvents.filter((ev) =>
+    ev.signature.includes("Burn")
+  );
+
+  var { account, id } = event[0].params;
+
+  var bbitesTokenAdd = "0xE255E418cd36f9F6Eae5374aABE0fB8b8D0ED302";
+  var tokenAbi = ["function mint(address to, uint256 amount)"];
+  var tokenContract = new ethers.Contract(bbitesTokenAdd, tokenAbi, signer);
+  var tokens = ethers.utils.parseUnits("10000", 18);
+  var tx = await tokenContract.mint(account, tokens);
+  var res = await tx.wait();
+  return res;
 };
